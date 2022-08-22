5843d155c672205a750e694dbeee79cdc256ec93
nemurihimee <ckandrew04@gmail.com>
2022-08-22 14:04:09 -0700

    Initial Commit

---
+++ WRID/WRIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  WRIDApp.swift
+//  WRID
+//
+//  Created by Minjun Kim on 8/22/22.
+//
+
+import SwiftUI
+
+@main
+struct WRIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
